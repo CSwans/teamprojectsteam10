1cf06f9e61641dd7e60bb97d789bca34a14d5e5d
March1703 <i.therdchanakul-13@student.lboro.ac.uk>
2015-02-11 15:53:41 +0000

    Insert fix

--- insertInfo.php
+++ insertInfo.php
@@ -55,6 +55,9 @@
 	$duration=$_POST['duration'];
 	
 	$weekInsert="";
+	$weekInsert2="";
+	$weekInsert3="";
+	$weekInsert4="";
 	$defaultWeeks = array(1,2,3,4,5,6,7,8,9,10,11,12);
 	
 	//connects to the database using the username and password
