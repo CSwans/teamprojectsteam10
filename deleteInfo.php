f34e2413b5b34a44de1ba1a544c2da00a2427190
March1703 <i.therdchanakul-13@student.lboro.ac.uk>
2015-02-12 22:22:48 +0000

    prepare structure to accommodate delete function

---
+++ deleteInfo.php
@@ -0,0 +1,21 @@
+<?php
+	//Starts the session, if there is not any sessions then it will transfer to the login page and the user will ave to log in again
+	//Inthuch Therdchanakul
+	
+	session_start();
+	if(!isset($_SESSION['username']) || !isset($_SESSION['password'])) {
+		header('Location: login.html');
+	}
+	//connects to the database using the username and passoword
+	require_once "MDB2.php";
+	$host = "co-project.lboro.ac.uk"; //host name
+	$dbName = "team10"; //database name
+	$dsn = "mysql://team10:abg83rew@$host/$dbName"; //login information
+	$db =& MDB2::connect($dsn); //connecting to the server and connecting to the database
+	if(PEAR::isError($db)){ //if we couldnt connect then end the connection
+		die($db->getMessage());
+	}
+	$db->setFetchMode(MDB2_FETCHMODE_ASSOC);
+	//username is the uppercase dept code that was loggged in
+	$username = strtoupper($_SESSION['username']);
+?>
