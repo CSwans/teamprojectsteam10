1f2eff24e517f1b52b4d1d5743f4de3dcbc83c99
March1703 <i.therdchanakul-13@student.lboro.ac.uk>
2015-02-13 13:08:02 +0000

    Update updateInfo.php
    now update database when clicking edit

--- updateInfo.php
+++ updateInfo.php
@@ -17,5 +17,72 @@
 	}
 	$db->setFetchMode(MDB2_FETCHMODE_ASSOC);
 	//username is the uppercase dept code that was loggged in
-	$username = strtoupper($_SESSION['username']);
+	
+	$deptCode = strtoupper($_SESSION['username']);
+	$request_id = $_POST['requestId'];
+	$moduleCode = $_POST['module_code_select'];
+	$roomCode = $_POST['roomCode0'];
+	$cap1=$_POST['capacity'];
+	$wheelchair=$_POST['wheelchair'];
+	$projector=$_POST['projector'];
+	$visualiser=$_POST['visualiser'];
+	$whiteboard=$_POST['whiteboard'];
+	$priority=$_POST['priorityInput'];
+	$priority=$_POST['priorityInput'];
+	$specialRequirements="";
+	if(isset($_POST['specialReq'])) 
+		$specialRequirements=$_POST['specialReq'];
+	
+	$period=$_POST['time'];
+	$days=array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday" );
+	$dayNo=$_POST['day'];
+	$day=$days[$dayNo-1];
+	$duration=$_POST['duration'];
+	$weekInsert="";
+	$defaultWeeks = array(1,2,3,4,5,6,7,8,9,10,11,12);
+	
+	$sql1 = "UPDATE REQUEST SET module_code='$moduleCode', room_code='$roomCode', capacity=$cap1, wheelchair=$wheelchair, projector=$projector, visualiser=$visualiser, whiteboard=$whiteboard, special_requirements='$specialRequirements',
+	 priority=$priority, period=$period, day='$day', duration=$duration WHERE request_id=$request_id";
+	$res =& $db->query($sql1); //query the result from the database
+	if(PEAR::isError($res)){
+		die($res->getMessage());
+	}
+	
+	$sql2 = "DELETE FROM REQUEST_WEEKS WHERE request_id=$request_id";
+	$res =& $db->query($sql2); //query the database
+	if(PEAR::isError($res)){
+		die($res->getMessage());
+	}
+	
+	if(!empty($_POST['weeks'])){ //checks to see if it is the default week format (1-12) if it is then it will put 0 in the REQUEST_WEEKS table 
+		if(sizeof(array_diff($_POST['weeks'],$defaultWeeks)) != 0 || sizeof(array_diff($defaultWeeks,$_POST['weeks'])) != 0) {
+			foreach($_POST['weeks'] as $weeks){
+				$weekInsert = 'INSERT INTO REQUEST_WEEKS(request_id, week) VALUES (' . $request_id . ',' . $weeks . '); ';
+				$res =& $db->query($weekInsert); 
+				if(PEAR::isError($res)){
+					die($res->getMessage());
+				} 
+		
+			}
+		}
+	}	
+	else{
+		$weekInsert = 'INSERT INTO REQUEST_WEEKS (request_id, week) VALUES (' . $request_id . ',0); ';
+		$res =& $db->query($weekInsert); 
+		if(PEAR::isError($res)){
+			die($res->getMessage());
+		} 
+	}
+	
+	$sql5 = "SELECT * FROM REQUEST";
+	$res =& $db->query($sql5); 
+	if(PEAR::isError($res)){
+		die($res->getMessage());
+	}
+	$results = array();
+	while($row = $res->fetchRow()){
+		$results[] = $row;
+	}
+	
+	echo json_encode($results);
 ?>
